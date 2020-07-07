ab34ec2a6c06d37d20494b7f45c6022483d4ede9
anythinglord <sjsalcedoa@unal.edu.co>
2020-07-06 21:24:07 -0500

    Updates

---
+++ precache-manifest.898989788ddddf7d3b9171cb74e7bf27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d472bee80cd46f5e8175bbc1ccb841",
+    "url": "/covid-deploy/index.html"
+  },
+  {
+    "revision": "c61d473b34fd9cee5d3b",
+    "url": "/covid-deploy/static/css/main.37668ccf.chunk.css"
+  },
+  {
+    "revision": "a759e9061a6b92942b5f",
+    "url": "/covid-deploy/static/js/2.a6a5f424.chunk.js"
+  },
+  {
+    "revision": "809646f9bc31af8cda07de01f8b33510",
+    "url": "/covid-deploy/static/js/2.a6a5f424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c61d473b34fd9cee5d3b",
+    "url": "/covid-deploy/static/js/main.a9c2b4e2.chunk.js"
+  },
+  {
+    "revision": "f580251a2664d60f642e",
+    "url": "/covid-deploy/static/js/runtime-main.a29ae4bd.js"
+  }
+]);
